2324b2b4ce70b050a7ca8a1338c361dbdca62f3f
kmburu <keithmburu1@gmail.com>
2022-01-18 12:30:41 -0500

    done

---
+++ A00/fortune.c
@@ -0,0 +1,13 @@
+/***************************************************
+ * hello.c
+ * Author: Aline Normoyle
+ * Author: Keith Mburu
+ * Print fortune
+ */
+
+#include <stdio.h>
+
+int main() {
+  printf("Actions speak louder than words\n");
+  return 0;
+}
