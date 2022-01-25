0808548b2a38ea7d3a70e3d31b9fc780cc2ac710
kmburu <keithmburu1@gmail.com>
2022-01-24 22:35:55 -0500

    password comments

--- A01/password.c
+++ A01/password.c
@@ -1,11 +1,26 @@
+/*
+password.c
+Author: Keith Mburu
+1/28/2022
+Convert password to bad password
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+#define MAXLENGTH 10 // maximum password length
+
+/* 
+Converts password given by the user to a bad password
+password: input from user
+size: length of password
+badpassword: output to be constructed
+*/
 void bad_password(char password[], int size, char badpassword[]) {
   for (int i = 0; i < size; i++) {
-    switch(password[i]) {
-      case 'e':
+    switch(password[i]) { 
+      case 'e': // replace
         badpassword[i] = '3';
         break;
       case 'l':
@@ -14,19 +29,19 @@ void bad_password(char password[], int size, char badpassword[]) {
       case 'a':
         badpassword[i] = '@';
         break;
-      default:
+      default:  // copy
         badpassword[i] = password[i];
     }
   }
-  badpassword[size] = '\0';
+  badpassword[size] = '\0'; // terminate string
 }
 
 int main() {
-  char password[10];
-  char badpassword[10]; 
+  char password[MAXLENGTH];
+  char badpassword[MAXLENGTH]; 
   printf("Enter a word: ");
-  scanf("%s", &password);
-  bad_password(password, strlen(password), badpassword);
+  scanf("%s", &password); // polling for input
+  bad_password(password, strlen(password), badpassword); // constructing output
   printf("Your bad password is %s\n", badpassword);
   return 0;
 }
