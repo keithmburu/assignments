224919f62f9e059bc13ea006f40d917c97f7ac23
kmburu <keithmburu1@gmail.com>
2022-01-24 22:20:35 -0500

    rpc improved

--- A01/rpc.c
+++ A01/rpc.c
@@ -19,51 +19,52 @@ const char* ai_choice() {
   }
 }
 
+void ai_outcome(char aiMove[], char aiBeats[], char winMessage[], char lossMessage[]) {
+  if (strcmp(aiMove, "rock") == 0) {
+    strcpy(aiBeats, "scissors");
+    strcpy(winMessage, "Rock bashes scissors");
+    strcpy(lossMessage, "Paper covers rock");
+  } 
+  else if (strcmp(aiMove, "paper") == 0) {
+    strcpy(aiBeats, "rock");
+    strcpy(winMessage, "Paper cover rock");
+    strcpy(lossMessage, "Scissors cuts paper");
+  }
+  else {
+    strcpy(aiBeats, "paper");
+    strcpy(winMessage, "Scissors cut paper");
+    strcpy(lossMessage, "Rock bashes scissors");
+  }
+}
+
 void play(char move[], int scores[]) {
-  char aiMove[8];
+  char aiMove[9];
+  char aiBeats[9];
+  char winMessage[21];
+  char lossMessage[21];
   strcpy(aiMove, ai_choice());
   printf("AI chose %s", aiMove);
+  ai_outcome(aiMove, aiBeats, winMessage, lossMessage);
   if (strcmp(move, "rock") != 0 && strcmp(move, "paper") != 0 && strcmp(move, "scissors") != 0) {
     printf("\nYou entered an invalid choice: %s", move);
   }
-  else if (strcmp(move, aiMove) != 0) { 
-    if (strcmp(move, "scissors") == 0){
-      if (strcmp(aiMove, "paper") == 0) {
-        printf("\nScissors cut paper");
-        scores[1]++;
-      } 
-    }
-    else if (strcmp(move, "paper") == 0) {
-      if (strcmp(aiMove, "scissors") == 0) {
-        printf("\nScissors cut paper");
-        scores[0]++;
-      }
-    } 
-    else if (strcmp(move, aiMove) < 0) {
-      if (strcmp(move, "paper") == 0) {
-        printf("\nPaper covers rock");
-      } 
-      else {
-        printf("\nRock bashes scissors");
-      }
-      scores[1]++;
-    }
-    else if (strcmp(move, aiMove) > 0) {
-      if (strcmp(aiMove, "paper") == 0) {
-        printf("\nPaper covers rock");
-      } 
-      else {
-        printf("\nRock bashes scissors");
-      }
-      scores[0]++;
-    }
+  else if (strcmp(aiBeats, move) == 0) {
+    scores[0]++;
+    printf("\n%s", winMessage);
+  }
+  else if (strcmp(move, aiMove) == 0) {
+    ;
+  }
+  else {
+    scores[1]++;
+    printf("\n%s", lossMessage);
   }
   printf("\nAI score: %d, Player score: %d\n", scores[0], scores[1]);
 }     
 
 int main() {
   int rounds;
-  char move[8];
+  char move[9];
   int scores[2];
   printf("Welcome to Rock, Paper, Scissors!\nHow many rounds do you want to play? ");
   scanf("%d", &rounds);
