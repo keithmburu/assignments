a22ee42e31cb4010a9975a8184c058d3dd8ebac2
kmburu <keithmburu1@gmail.com>
2022-01-29 18:08:22 -0500

    working

--- A02/repeat.c
+++ A02/repeat.c
@@ -1,5 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+void repeat(char* word, int count, char* repeated) {
+  for (int i = 0; i < count; i++) {
+    strcat(repeated, word);
+  }
+  printf("Your word is %s\n", repeated);
+}
 int main() {
+  char word[32];
+  int count;
+  printf("Enter a word: ");
+  scanf("%s", word); 
+  printf("Enter a count: ");
+  scanf("%d", &count);
+  char* repeated = malloc(strlen(word) * count);
+  if (repeated == NULL) {
+    printf("Cannot allocate new string. Exiting...\n");
+    exit(1);
+  } else {
+      repeat(word, count, repeated);
+      free(repeated);
+  }
   return 0;
 }
