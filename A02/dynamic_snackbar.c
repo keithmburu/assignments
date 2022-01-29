dc39b8096aa7d41966384dad876029f130e076d0
kmburu <keithmburu1@gmail.com>
2022-01-29 18:08:38 -0500

    working

--- A02/dynamic_snackbar.c
+++ A02/dynamic_snackbar.c
@@ -1,13 +1,67 @@
+/* 
+snackbar.c
+Author: Keith Mburu
+2/4/2022
+Maintain list of snacks available for purchase
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+#define MAXLENGTH 20 // maximum snack name length
+#define MAXNUMBER 10 // maximum number of snacks
+
+/*
+Struct with snack attributes
+*/
 struct snack {
   char name[32];
   float cost;
   int quantity;
 };
 
+/*
+Constructs the struct corresponding to each snack
+name: snack name
+cost: snack cost
+quantity: snack quantity
+returns: snack struct
+*/
+struct snack snack_struct(char name[], float cost, int quantity) {
+  struct snack x;
+  strcpy(x.name, name);
+  x.cost = cost;
+  x.quantity = quantity;
+  return x;
+}
+
 int main() {
+  struct snack snacks[MAXNUMBER]; // array of structs
+  int adding;
+  char name[MAXLENGTH];
+  float cost;
+  int quantity;
+  int options = 0;
+  
+  printf("Enter a number of snacks: ");
+  scanf("%d", &adding);
+  for (int i = 0; i < adding; i++) {
+    printf("Enter a name: ");
+    scanf("%s", name);
+    printf("Enter a cost: ");
+    scanf("%f", &cost);
+    printf("Enter a quantity: ");
+    scanf("%d", &quantity);
+    snacks[i] = snack_struct(name, cost, quantity);
+    options++;
+  }
+  
+  printf("\nWelcome to Dynamic Donna's Snack Bar.\n\n");
+  for (int i = 0; i < options; i++) {
+    printf("%d) %-20s cost: $%-10.2f quantity: %d\n", i, snacks[i].name, 
+      snacks[i].cost, snacks[i].quantity);
+  }
+
   return 0;
 }
