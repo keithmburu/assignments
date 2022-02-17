6025d35f43d45ececb4ed95b604b94e2c132a7fa
kmburu <keithmburu1@gmail.com>
2022-02-16 20:24:18 -0500

    comments

--- A04/ascii_image.c
+++ A04/ascii_image.c
@@ -1,7 +1,56 @@
+/*
+ascii_image.c
+Author: Keith Mburu
+2/18/2022
+Represents RGB image using ASCII
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "read_ppm.h"
+
+void print_image(struct ppm_pixel** raster, int width, int height) {
+  for (int i=0; i < height; i++) {
+    for (int j=0; j < width; j++) {
+      int total = raster[i][j].red + raster[i][j].green + raster[i][j].blue;
+      int average = total/3;
+      char symbol = '\0';
+      if (0 <= average && average <= 25) {
+        symbol = '@';
+      }
+      else if (26 <= average && average <= 50) {
+        symbol = '#';
+      }
+      else if (51 <= average && average <= 75) {
+        symbol = '%';
+      }
+      else if (76 <= average && average <= 100) {
+        symbol = '*';
+      }
+      else if (101 <= average && average <= 125) {
+        symbol = 'o';
+      }
+      else if (126 <= average && average <= 150) {
+        symbol = ';';
+      }
+      else if (151 <= average && average <= 175) {
+        symbol = ':';
+      }
+      else if (176 <= average && average <= 200) {
+        symbol = ',';
+      }
+      else if (201 <= average && average <= 225) {
+        symbol = '.';
+      }
+      else if (226 <= average && average <= 255) {
+        symbol = ' ';
+      }
+      printf("%c", symbol);
+    }
+  printf("\n");
+  }  
+}
 
 int main(int argc, char** argv) {
   if (argc != 2) {
@@ -15,52 +64,14 @@ int main(int argc, char** argv) {
   }
   char filename[32] = "./";
   strcat(filename, argv[1]);
+
   int width;
   int height;
-  struct ppm_pixel** raster = read_ppm(filename, &width, &height);
-  if (raster == NULL) {
-    exit(2);
-  }
-  printf("Reading %s.ppm with width %d and height %d\n", filename, width, height);
-  for (int i=0; i < height; i++) {
-    for (int j=0; j < width; j++) {
-      int total = raster[i][j].red + raster[i][j].green + raster[i][j].blue;
-      int average = total/3;
-      char symbol;
-      if (average <= 25) {
-        symbol = '@';
-      }
-      else if (average <= 50) {
-        symbol = '#';
-      }
-      else if (average <= 75) {
-        symbol = '%';
-      }
-      else if (average <= 100) {
-        symbol = '*';
-      }
-      else if (average <= 125) {
-        symbol = 'o';
-      }
-      else if (average <= 150) {
-        symbol = ';';
-      }
-      else if (average <= 175) {
-        symbol = ':';
-      }
-      else if (average <= 200) {
-        symbol = ',';
-      }
-      else if (average <= 225) {
-        symbol = '.';
-      }
-      else if (average <= 255) {
-        symbol = ' ';
-      }
-      printf("%c", symbol);
-    }
-  printf("\n");
-  }  
+  struct ppm_pixel** raster = read_ppm(filename, &width, &height); // matrix
+
+  printf("Reading %s.ppm with width %d and height %d\n", filename, width, 
+    height);
+  print_image(raster, width, height);
   return 0;
 }
 
--- A04/read_ppm.c
+++ A04/read_ppm.c
@@ -1,12 +1,23 @@
+/*
+read_ppm.c
+Author: Keith Mburu
+2/18/2022
+Reads in a ppm file and formats the contents as a matrix of pixels
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "read_ppm.h"
 
-#define BUFFERSIZE 1024
+#define BUFFERSIZE 64
 
-
-// 
+/*
+Reads in a ppm file and formats the contents as a matrix of pixels
+filename: name of file to be read in
+w: width of correponding image
+h: height of corresponding image
+returns pointer to the matrix of pixels
+*/
 struct ppm_pixel** read_ppm(const char* filename, int* w, int* h) {
   FILE* file = fopen(filename, "r");
   if (file == NULL) {
@@ -15,35 +26,40 @@ struct ppm_pixel** read_ppm(const char* filename, int* w, int* h) {
   }
   char* buffer = malloc(sizeof(char) * BUFFERSIZE);
   memset(buffer, '\0', BUFFERSIZE);
-  
-  struct ppm_pixel** raster = NULL;
-  
-  fgets(buffer, BUFFERSIZE, file);
-  printf("%s\n", buffer);
+   
+  fgets(buffer, BUFFERSIZE, file); // "magic number"
+  printf("%s\n", buffer); 
   memset(buffer, '\0', BUFFERSIZE);
   
-  fgets(buffer, BUFFERSIZE, file);
+  fgets(buffer, BUFFERSIZE, file); // comment or width, height
   if (buffer[0] == '#') {
+    memset(buffer, '\0', BUFFERSIZE);
+    printf("%s\n", buffer); 
     fgets(buffer, BUFFERSIZE, file);   
   }
   printf("%s\n", buffer);
   sscanf(buffer, " %d %d ", w, h);
-  raster = malloc(sizeof(struct ppm_pixel*)* *h);
-  for (int i=0; i < *h; i++) {
-    raster[i] = malloc(sizeof(struct ppm_pixel)* *w);
-  }
+  struct ppm_pixel** raster = malloc(sizeof(struct ppm_pixel*)* *h); // matrix
   if (raster == NULL) {
+    free(buffer);
     fclose(file);
     return NULL;
   }
+  for (int i=0; i < *h; i++) {
+    raster[i] = malloc(sizeof(struct ppm_pixel)* *w);
+    if (raster[i] == NULL) {
+      free(buffer);
+      fclose(file);
+      return NULL;
+    }
+  }
   memset(buffer, '\0', BUFFERSIZE);
   
-  fgets(buffer, BUFFERSIZE, file);
+  fgets(buffer, BUFFERSIZE, file); // maxval
   printf("%s\n", buffer);
-  int maxval;
-  sscanf(buffer, " %d ", &maxval);
+  memset(buffer, '\0', BUFFERSIZE);
 
-  for (int i=0; i < *h; i++) {
+  for (int i=0; i < *h; i++) { // pixels
     for (int j=0; j < *w; j++) { 
       unsigned char r;
       unsigned char g;
