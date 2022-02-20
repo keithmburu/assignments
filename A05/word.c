06d676523c412cfce4cb3c855bc7f31cc889d01a
kmburu <keithmburu1@gmail.com>
2022-02-20 17:28:18 -0500

    working

--- A05/word.c
+++ A05/word.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
 int main() {
+  printf("Enter 4 characters: ");
+  char word[4];
+  scanf("%s", word);
+  unsigned int result = 0x00000000;
+  for (int i=3; i != -1; i--) {
+    unsigned int hex = word[i]<<8*(3-i);
+    printf("Debug: %c = 0x%08X\n", word[i], hex);
+    result += hex;
+  }
+  printf("Your number is: %d (0x%X)\n", result, result);
   return 0;
 }
