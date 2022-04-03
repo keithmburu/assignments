e126ee86cc1b9539ef7d71d2cddfab9ea23107e9
kmburu <keithmburu1@gmail.com>
2022-04-03 19:07:19 -0400

    started

--- A09/multi_mandelbrot.c
+++ A09/multi_mandelbrot.c
@@ -8,6 +8,8 @@
 #include <sys/shm.h>
 #include <sys/ipc.h>
 #include "read_ppm.h"
+#include <string.h>
+#include <math.h>
 
 int main(int argc, char* argv[]) {
   int size = 480;
@@ -19,7 +21,7 @@ int main(int argc, char* argv[]) {
   int numProcesses = 4;
 
   int opt;
-  while ((opt = getopt(argc, argv, ":s:l:r:t:b:p:")) != -1) {
+  while ((opt = getopt(argc, argv, ":s:l:r:t:b:")) != -1) {
     switch (opt) {
       case 's': size = atoi(optarg); break;
       case 'l': xmin = atof(optarg); break;
@@ -35,7 +37,96 @@ int main(int argc, char* argv[]) {
   printf("  X range = [%.4f,%.4f]\n", xmin, xmax);
   printf("  Y range = [%.4f,%.4f]\n", ymin, ymax);
 
-  // todo: your code here
+  // todo: your work here
+  struct ppm_pixel* raster = malloc(sizeof(struct ppm_pixel) * size * size);
+  memset(raster, 0, (sizeof(struct ppm_pixel) * size * size));
+
+  struct timeval tstart, tend;
+  double timer;
+
+  srand(time(0));
+
+  gettimeofday(&tstart, NULL);
   // generate pallet
+  struct ppm_pixel* palette = malloc(maxIterations * sizeof(struct ppm_pixel));
+  memset(palette, 0, maxIterations * sizeof(struct ppm_pixel));
+  for (int i=0; i < maxIterations; i++) {
+    struct ppm_pixel color;
+    color.red = rand() % 255;
+    color.green = rand() % 255;
+    color.blue = rand() % 255;
+    palette[i] = color;
+  }
+  struct ppm_pixel black = {{{0, 0, 0}}};
+
+  int* pids = malloc(sizeof(int) * numProcesses);
+  memset(pids, 0, sizeof(int) * numProcesses);
+  int rowstart = 0;
+  int rowend = size/sqrt(numProcesses);
+  int colstart = 0;
+  int colend = size/sqrt(numProcesses);
+  for (int i = 0; i < numProcesses; i++) {
+    int pid = fork();
+    if (pid == 0) { 
+      break; 
+    }
+    pids[i] = pid;
+    printf("Launched child process: %d\n", pid);
+    printf("%d) Sub-image block: cols (%d, %d) to rows (%d,%d)\n", pid, 
+      colstart, colend, rowstart, rowend);
+    if (i == numProcesses-1) {
+      for (int i = 0; i < numProcesses; i++) {
+        int status;
+        waitpid(pids[i], &status, 0);
+        printf("Child process complete: %d\n", pids[i]);
+      }
+      gettimeofday(&tend, NULL);
+      timer = tend.tv_sec - tstart.tv_sec + (tend.tv_usec - tstart.tv_usec)/1.e6;
+      printf("Computed mandelbrot set (%dx%d) in %.6g seconds\n", size, size, timer);
+      char filename[32];
+      sprintf(filename, "multi-mandelbrot-%d-%ld.ppm", size, time(0));
+      write_ppm(filename, raster, size, size);
+      exit(0);
+    }
+    if (i == sqrt(numProcesses)-1) {
+      rowstart += size/sqrt(numProcesses);
+      rowend += size/sqrt(numProcesses);
+      colstart = 0;
+      colend = size/sqrt(numProcesses);
+    } else {
+      colstart += size/sqrt(numProcesses);
+      colend += size/sqrt(numProcesses);
+    }
+  }
   // compute image
+  for (int row = 0; row < size; row++) {
+    for (int col = 0; col < size; col++) {
+      int xfrac = row / size;
+      int yfrac = col / size;
+      int x0 = xmin + xfrac * (xmax - xmin);
+      int y0 = ymin + yfrac * (ymax - ymin);
+
+      int x = 0;
+      int xtmp;
+      int y = 0;
+      int iter = 0;
+      while ((iter < maxIterations) && ((x*x + y*y) < 4*4)) {
+        xtmp = x*x - y*y + x0;
+        y = 2*x*y + y0;
+        x = xtmp;
+        iter++;
+        //printf("x: %d y: %d iter: %d\n", x, y, iter);
+      }
+      struct ppm_pixel color;
+      if (iter < maxIterations) { // escaped
+        color = palette[iter];
+        //printf("r: %d g: %d b: %d\n", color.red, color.green, color.blue);
+      } else {
+        color = black;
+      }
+      // write color to image at location (row,col)
+      raster[(row*size)+col] = color;
+    }
+  }
+  return 0;
 }
