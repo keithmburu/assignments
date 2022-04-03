4114ef0dbbc5b60f240aaa32d05a3df086e27035
kmburu <keithmburu1@gmail.com>
2022-04-03 18:21:55 -0400

    started

--- A09/single_mandelbrot.c
+++ A09/single_mandelbrot.c
@@ -4,6 +4,7 @@
 #include <time.h>
 #include <sys/time.h>
 #include "read_ppm.h"
+#include <string.h>
 
 int main(int argc, char* argv[]) {
   int size = 480;
@@ -14,14 +15,20 @@ int main(int argc, char* argv[]) {
   int maxIterations = 1000;
 
   int opt;
+  int i = 0;
+  char usage[128] = "\0";
+  strcat(usage, argv[0]);
   while ((opt = getopt(argc, argv, ":s:l:r:t:b:")) != -1) {
+    i++;
+    strcat(usage, " ");
+    strcat(usage, argv[i]);
     switch (opt) {
       case 's': size = atoi(optarg); break;
       case 'l': xmin = atof(optarg); break;
       case 'r': xmax = atof(optarg); break;
       case 't': ymax = atof(optarg); break;
       case 'b': ymin = atof(optarg); break;
-      case '?': printf("usage: %s -s <size> -l <xmin> -r <xmax> -b <ymin> -t <ymax>\n", argv[0]); break;
+      case '?': printf("usage: %s\n", usage); break;
     }
   }
   printf("Generating mandelbrot with size %dx%d\n", size, size);
@@ -29,9 +36,62 @@ int main(int argc, char* argv[]) {
   printf("  Y range = [%.4f,%.4f]\n", ymin, ymax);
 
   // todo: your work here
-  // generate pallet
+  struct ppm_pixel* raster = malloc(sizeof(struct ppm_pixel) * size * size);
+  memset(raster, 0, (sizeof(struct ppm_pixel) * size * size));
+
+  struct timeval tstart, tend;
+  double timer;
+
   srand(time(0));
 
+  gettimeofday(&tstart, NULL); 
+  // generate pallet
+  struct ppm_pixel* palette = malloc(maxIterations * sizeof(struct ppm_pixel));
+  memset(palette, 0, maxIterations * sizeof(struct ppm_pixel));
+  for (int i=0; i < maxIterations; i++) {
+    struct ppm_pixel color;
+    color.red = rand() % 255;
+    color.green = rand() % 255;
+    color.blue = rand() % 255;
+    palette[i] = color;
+  }
+  struct ppm_pixel black = {{{0, 0, 0}}};
   // compute image
+  for (int row = 0; row < size; row++) {
+    for (int col = 0; col < size; col++) {
+      int xfrac = row / size;
+      int yfrac = col / size;
+      int x0 = xmin + xfrac * (xmax - xmin);
+      int y0 = ymin + yfrac * (ymax - ymin);
 
+      int x = 0;
+      int xtmp;
+      int y = 0;
+      int iter = 0;
+      while ((iter < maxIterations) && ((x*x + y*y) < 4*4)) {
+        xtmp = x*x - y*y + x0;
+        y = 2*x*y + y0;
+        x = xtmp;
+        iter++;
+        printf("x: %d y: %d iter: %d\n", x, y, iter);
+      }
+      struct ppm_pixel color; 
+      if (iter < maxIterations) { // escaped
+        color = palette[iter];
+        //printf("r: %d g: %d b: %d\n", color.red, color.green, color.blue);
+      } else {
+        color = black;
+      }
+      // write color to image at location (row,col)
+      raster[(row*size)+col] = color;
+    }
+  }
+  gettimeofday(&tend, NULL);
+  
+  timer = tend.tv_sec - tstart.tv_sec + (tend.tv_usec - tstart.tv_usec)/1.e6;
+  
+  printf("Computed mandelbrot set (%dx%d) in %.6g seconds\n", size, size, timer);
+  char filename[32];
+  sprintf(filename, "mandelbrot-%d-%ld.ppm", size, time(0));
+  write_ppm(filename, raster, size, size);
 }
