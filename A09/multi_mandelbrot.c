56fc7016b09bb79f10470a63d2274e1a44dc41ce
kmburu <keithmburu1@gmail.com>
2022-04-04 00:49:59 -0400

    working except shared mem

--- A09/multi_mandelbrot.c
+++ A09/multi_mandelbrot.c
@@ -28,6 +28,7 @@ int main(int argc, char* argv[]) {
       case 'r': xmax = atof(optarg); break;
       case 't': ymax = atof(optarg); break;
       case 'b': ymin = atof(optarg); break;
+      case 'p': numProcesses = atoi(optarg); break;
       case '?': printf("usage: %s -s <size> -l <xmin> -r <xmax> "
         "-b <ymin> -t <ymax> -p <numProcesses>\n", argv[0]); break;
     }
@@ -61,54 +62,69 @@ int main(int argc, char* argv[]) {
 
   int* pids = malloc(sizeof(int) * numProcesses);
   memset(pids, 0, sizeof(int) * numProcesses);
+
   int rowstart = 0;
   int rowend = size/sqrt(numProcesses);
   int colstart = 0;
   int colend = size/sqrt(numProcesses);
+  int** rowscols = malloc(sizeof(int*) * numProcesses);
   for (int i = 0; i < numProcesses; i++) {
-    int pid = fork();
-    if (pid == 0) { 
-      break; 
-    }
-    pids[i] = pid;
-    printf("Launched child process: %d\n", pid);
-    printf("%d) Sub-image block: cols (%d, %d) to rows (%d,%d)\n", pid, 
-      colstart, colend, rowstart, rowend);
-    if (i == numProcesses-1) {
-      for (int i = 0; i < numProcesses; i++) {
-        int status;
-        waitpid(pids[i], &status, 0);
-        printf("Child process complete: %d\n", pids[i]);
-      }
-      gettimeofday(&tend, NULL);
-      timer = tend.tv_sec - tstart.tv_sec + (tend.tv_usec - tstart.tv_usec)/1.e6;
-      printf("Computed mandelbrot set (%dx%d) in %.6g seconds\n", size, size, timer);
-      char filename[32];
-      sprintf(filename, "multi-mandelbrot-%d-%ld.ppm", size, time(0));
-      write_ppm(filename, raster, size, size);
-      exit(0);
-    }
+    rowscols[i] = malloc(sizeof(int) * 4); 
+    rowscols[i][0] = rowstart;                             
+    rowscols[i][1] = rowend;  
+    rowscols[i][2] = colstart;                         
+    rowscols[i][3] = colend;   
     if (i == sqrt(numProcesses)-1) {
-      rowstart += size/sqrt(numProcesses);
-      rowend += size/sqrt(numProcesses);
-      colstart = 0;
-      colend = size/sqrt(numProcesses);
-    } else {
       colstart += size/sqrt(numProcesses);
       colend += size/sqrt(numProcesses);
+      rowstart = 0;
+      rowend = size/sqrt(numProcesses);
+    } else {
+      rowstart += size/sqrt(numProcesses);
+      rowend += size/sqrt(numProcesses);
+    }
+  }
+  for (int i = 0; i < numProcesses; i++) {
+    int pid = fork();
+    if (pid == 0) {   
+      rowstart = rowscols[i][0];                              
+      rowend = rowscols[i][1];  
+      colstart = rowscols[i][2] ;                         
+      colend = rowscols[i][3] ;   
+      break; 
+    } else {
+      pids[i] = pid;
+      printf("Launched child process: %d\n", pid);
+      printf("%d) Sub-image block: cols (%d, %d) to rows (%d,%d)\n", pid, 
+        rowscols[i][2], rowscols[i][3], rowscols[i][0], rowscols[i][1]);
+      if (i == numProcesses-1) {
+        for (int i = 0; i < numProcesses; i++) {
+          int status;
+          waitpid(pids[i], &status, 0);
+          printf("Child process complete: %d\n", pids[i]);
+        }
+        gettimeofday(&tend, NULL);
+        timer = tend.tv_sec - tstart.tv_sec + (tend.tv_usec - tstart.tv_usec)/1.e6;
+        printf("Computed mandelbrot set (%dx%d) in %.6g seconds\n", size, size, timer);
+        char filename[32];
+        sprintf(filename, "multi-mandelbrot-%d-%ld.ppm", size, time(0));
+        write_ppm(filename, raster, size, size);
+        exit(0);
+      }
     }
   }
   // compute image
-  for (int row = 0; row < size; row++) {
-    for (int col = 0; col < size; col++) {
-      int xfrac = row / size;
-      int yfrac = col / size;
-      int x0 = xmin + xfrac * (xmax - xmin);
-      int y0 = ymin + yfrac * (ymax - ymin);
+  for (int row = rowstart; row < rowend; row++) {
+    for (int col = colstart; col < colend; col++) {
+      float xfrac = (float) (row+1) / size;
+      float yfrac = (float) (col+1) / size;
+      float x0 = xmin + xfrac * (xmax - xmin);
+      float y0 = ymin + yfrac * (ymax - ymin);
+      //printf("xfrac: %f yfrac: %f x0: %f y0: %f\n", xfrac, yfrac, x0, y0);
 
-      int x = 0;
-      int xtmp;
-      int y = 0;
+      float x = 0;
+      float xtmp;
+      float y = 0;
       int iter = 0;
       while ((iter < maxIterations) && ((x*x + y*y) < 4*4)) {
         xtmp = x*x - y*y + x0;
